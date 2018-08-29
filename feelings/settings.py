bb97378057fb09cd63366d900fe02a8a2b987f51
Shashwat Koliwad <koliwad96@gmail.com>
2018-08-29 14:59:47 +0530

    creating Dashboard

--- feelings/settings.py
+++ feelings/settings.py
@@ -121,3 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'assets'),]
+
+#Authentication
+LOGIN_URL = 'users:login'
+LOGIN_REDIRECT_URL = 'users:dashboard'
--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.conf.urls import url,include
+from . import views
 
 urlpatterns = [
-    url('^', include('django.contrib.auth.urls'))
+    url('^', include('django.contrib.auth.urls')),
+    url(r'^dashboard/$', views.dashboard, name='dashboard')
 ]
